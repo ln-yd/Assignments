bd314e63a0bc148d6e4bf5732d290a7515d50ccc
Ln-yd <leslienduong@gmail.com>
2020-08-05 01:31:31 -0700

    solve BasicJava #5

--- theBasics/BasicJava.java
+++ theBasics/BasicJava.java
@@ -28,14 +28,54 @@ public class BasicJava{
             System.out.printf("New number: %d Sum: %d%n", i, sum);
         }
     }
+    // 4.Iterating through an array
+    // Given an array X, say [1,3,5,7,9,13], 
+    // write a method that would iterate through each member of the array 
+    // and print each value on the screen.
+    public static void iterateArr(int[] X){
+        for(int i=0; i<X.length; i++){
+            System.out.println(X[i]);
+        }
+
+    }
+    // 5.Find Max
+    // Write a method (sets of instructions) that takes any array and prints the maximum value in the array.
+    //  Your method should also work with a given array that has all negative numbers (e.g. [-3, -5, -7]),
+    //   or even a mix of positive numbers, negative numbers and zero.
+    public static void findMax(int[]X){
+        if(X.length ==0){
+            System.out.println("No array given");
+            return;
+        }
+        int max =X[0];
+        for(int i=1; i<X.length;i++){
+            if(X[i] > max){
+                max=X[i];
+            }
+            
+        }
+        System.out.println(max);
+    }
 
     public static void main(String[] args) {
-        System.out.println("HELLO");
-        print1To255();
-        printOdd1To255();
-        printNumSum0To255();
-
-
-        
+        // System.out.println("HELLO");
+        // print1To255();
+        // printOdd1To255();
+        // printNumSum0To255();
+        // iterateArr(new int[]{1,3,5,7,9,13});
+        findMax(new int[] {});
     }
+
+
+    // 6.Get Average
+    // Write a method that takes an array, and prints the AVERAGE 
+    // of the values in the array. For example for an array [2, 10, 3], 
+    // your method should print an average of 5. Again, make sure you 
+    // come up with a simple base case and write instructions to solve 
+    // that base case first, then test your instructions for other complicated cases.
+
+    // 7.Array with Odd Numbers
+    // Write a method that creates an array 'y' that contains all the 
+    // odd numbers between 1 to 255. When the method is done, 'y' 
+    // should have the value of [1, 3, 5, 7, ... 255].
 }
