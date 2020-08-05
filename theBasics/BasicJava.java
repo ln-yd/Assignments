ab27318f7f9d86ab20ec6fc704c895971cc1482a
Ln-yd <leslienduong@gmail.com>
2020-08-05 00:47:46 -0700

    solve BasicJava #3. untrack .class files

--- theBasics/BasicJava.java
+++ theBasics/BasicJava.java
@@ -10,22 +10,31 @@ public class BasicJava{
 
     // 2. Print odd numbers between 1-255
     // Write a method that prints all the odd numbers from 1 to 255.
+    public static void printOdd1To255(){
+        for(int i=1; i<=255; i+=2){
+            System.out.println(i);
+        }
+    }
 
 
     // 3. Print Sum
-    // Write a method that prints the numbers from 0 to 255, but this time print the sum of the numbers that have been printed so far. For example, your output should be something like this:
-
-    // New number: 0 Sum: 0
-    // New number: 1 Sum: 1
-    // New number: 2 Sum: 3
-    // New number: 3 Sum: 6
-    // ...
-    // New number: 255 Sum: ___
-    // Do NOT use an array to do this exercise.
+    // Write a method that prints the numbers from 0 to 255, but this 
+    // time print the sum of the numbers that have been printed so far. 
+    // For example, your output should be something like this:
+    public static void printNumSum0To255(){
+        int sum = 0;
+        for(int i=0; i<=255; i++){
+            sum = sum + i;
+            System.out.printf("New number: %d Sum: %d%n", i, sum);
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("HELLO");
         print1To255();
+        printOdd1To255();
+        printNumSum0To255();
+
 
         
     }
